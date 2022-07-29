d3fb32cc2b3befef824b9f995c6bd30c1b50b85a
N0rmalUser <vova.okunev25@gmail.com>
2022-07-29 16:47:08 +0300

    no

--- app/src/main/java/com/androidKazik/MainActivity.kt
+++ app/src/main/java/com/androidKazik/MainActivity.kt
@@ -27,27 +27,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         mButton1 = findViewById(R.id.button1)
+        mButton1.setOnClickListener { qwe(x=1) }
         mButton2 = findViewById(R.id.button2)
+        mButton2.setOnClickListener { qwe(x=2) }
         mButton3 = findViewById(R.id.button3)
+        mButton3.setOnClickListener { qwe(x=3) }
         mButton4 = findViewById(R.id.button4)
+        mButton4.setOnClickListener { qwe(x=4) }
         mButton5 = findViewById(R.id.button5)
+        mButton5.setOnClickListener { qwe(x=5) }
         mButton6 = findViewById(R.id.button6)
+        mButton6.setOnClickListener { qwe(x=6) }
         mButton7 = findViewById(R.id.button7)
+        mButton7.setOnClickListener { qwe(x=7) }
         mButton8 = findViewById(R.id.button8)
+        mButton8.setOnClickListener { qwe(x=8) }
         mButton9 = findViewById(R.id.button9)
+        mButton9.setOnClickListener { qwe(x=9) }
         mButton10 = findViewById(R.id.button10)
+        mButton10.setOnClickListener { qwe(x=10) }
         mChislo = findViewById(R.id.chislo)
         mTextView=findViewById(R.id.textview)
-        mButton1.setOnClickListener { qwe(x=1) }
-        mButton2.setOnClickListener { qwe(x=2) }
-        mButton3.setOnClickListener { qwe(x=3) }
-        mButton4.setOnClickListener { qwe(x=4) }
-        mButton5.setOnClickListener { qwe(x=5) }
-        mButton6.setOnClickListener { qwe(x=6) }
-        mButton7.setOnClickListener { qwe(x=7) }
-        mButton8.setOnClickListener { qwe(x=8) }
-        mButton9.setOnClickListener { qwe(x=9) }
-        mButton10.setOnClickListener { qwe(x=10) }
     }
 
     @SuppressLint("SetTextI18n")
@@ -62,11 +62,11 @@ class MainActivity : AppCompatActivity() {
         else{
             Toast.makeText(this, messageLoss, Toast.LENGTH_SHORT).show()
         }
-        mTextView.text = when(mIndex) {             // мне было лень придумыватб или искать функцию для определения, будет ли в конце слова стоять "а" или нет.
-            2,3,4,22,23,24, ->"Ты выиграл $mIndex раза"
+        when(mIndex) {// мне было лень придумывать или искать функцию для определения, будет ли в конце слова стоять "а" или нет.
+            2,3,4,22,23,24->"Ты выиграл $mIndex раза"
             0,1,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,25,26,27,28,29,30->"Ты выиграл $mIndex раз"
             else -> ("Ты выиграл $mIndex раз(а)")
-        }
+        }.also { mTextView.text = it }
         mChislo.text = "Ты выбрал: $x \nПравильный ответ: $rand"
     }
 }
