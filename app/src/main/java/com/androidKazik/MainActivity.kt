6195b9eb7bfca4a4307d15eb58865cb43b2c170b
N0rmalUser <vova.okunev25@gmail.com>
2022-07-29 17:56:13 +0300

    no

--- app/src/main/java/com/androidKazik/MainActivity.kt
+++ app/src/main/java/com/androidKazik/MainActivity.kt
@@ -7,7 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
     private lateinit var mButton1: Button
     private lateinit var mButton2: Button
     private lateinit var mButton3: Button
@@ -20,46 +20,43 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mButton10: Button
     private lateinit var mTextView: TextView
     private lateinit var mChislo: TextView
-    private var mIndex = 0
+    var mIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mButton1 = findViewById(R.id.button1)
-        mButton1.setOnClickListener { qwe(x = 1) }
+        mButton1.setOnClickListener { qwe(1) }
         mButton2 = findViewById(R.id.button2)
-        mButton2.setOnClickListener { qwe(x = 2) }
+        mButton2.setOnClickListener { qwe(2) }
         mButton3 = findViewById(R.id.button3)
-        mButton3.setOnClickListener { qwe(x = 3) }
+        mButton3.setOnClickListener { qwe(3) }
         mButton4 = findViewById(R.id.button4)
-        mButton4.setOnClickListener { qwe(x = 4) }
+        mButton4.setOnClickListener { qwe(4) }
         mButton5 = findViewById(R.id.button5)
-        mButton5.setOnClickListener { qwe(x = 5) }
+        mButton5.setOnClickListener { qwe(5) }
         mButton6 = findViewById(R.id.button6)
-        mButton6.setOnClickListener { qwe(x = 6) }
+        mButton6.setOnClickListener { qwe(6) }
         mButton7 = findViewById(R.id.button7)
-        mButton7.setOnClickListener { qwe(x = 7) }
+        mButton7.setOnClickListener { qwe(7) }
         mButton8 = findViewById(R.id.button8)
-        mButton8.setOnClickListener { qwe(x = 8) }
+        mButton8.setOnClickListener { qwe(8) }
         mButton9 = findViewById(R.id.button9)
-        mButton9.setOnClickListener { qwe(x = 9) }
+        mButton9.setOnClickListener { qwe(9) }
         mButton10 = findViewById(R.id.button10)
-        mButton10.setOnClickListener { qwe(x = 10) }
+        mButton10.setOnClickListener { qwe(10) }
         mChislo = findViewById(R.id.chislo)
         mTextView = findViewById(R.id.textview)
     }
-
     @SuppressLint("SetTextI18n")
-    private fun qwe(x: Int) {
+    fun qwe(x: Int) {
         val rand = (1..10).random()
-        val messageWin = R.string.win
-        val messageLoss = R.string.loss
         if (x == rand) {
             mIndex += 1
-            Toast.makeText(this, messageWin, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, R.string.win, Toast.LENGTH_SHORT).show()
         } else {
-            Toast.makeText(this, messageLoss, Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, R.string.loss, Toast.LENGTH_SHORT).show()
         }
         when (mIndex) {// мне было лень придумывать или искать функцию для определения, будет ли в конце слова стоять "а" или нет.
             2, 3, 4, 22, 23, 24, -> "Ты выиграл $mIndex раза"
@@ -69,5 +66,3 @@ class MainActivity : AppCompatActivity() {
         mChislo.text = "Ты выбрал: $x \nПравильный ответ: $rand"
     }
 }
-
-
