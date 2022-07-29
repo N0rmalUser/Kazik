61daf4dd65133bdcc470cd74b0638769267e3c5a
N0rmalUser <vova.okunev25@gmail.com>
2022-07-29 16:10:18 +0300

    no

--- app/src/main/java/com/androidKazik/MainActivity.kt
+++ app/src/main/java/com/androidKazik/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var mButton9:Button
     private lateinit var mButton10:Button
     private lateinit var mTextView:TextView
+    private lateinit var mChislo:TextView
     private var mIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,56 +26,57 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         mButton1 = findViewById(R.id.button1)
+        mButton2 = findViewById(R.id.button2)
+        mButton3 = findViewById(R.id.button3)
+        mButton4 = findViewById(R.id.button4)
+        mButton5 = findViewById(R.id.button5)
+        mButton6 = findViewById(R.id.button6)
+        mButton7 = findViewById(R.id.button7)
+        mButton8 = findViewById(R.id.button8)
+        mButton9 = findViewById(R.id.button9)
+        mButton10 = findViewById(R.id.button10)
+        mChislo = findViewById(R.id.chislo)
+        mTextView=findViewById(R.id.textview)
         mButton1.setOnClickListener {
             val x=1
             qwe(x)
         }
-        mButton2 = findViewById(R.id.button2)
         mButton2.setOnClickListener {
             val x=2
             qwe(x)
         }
-        mButton3 = findViewById(R.id.button3)
         mButton3.setOnClickListener {
             val x=3
             qwe(x)
         }
-        mButton4 = findViewById(R.id.button4)
         mButton4.setOnClickListener {
             val x=4
             qwe(x)
         }
-        mButton5 = findViewById(R.id.button5)
         mButton5.setOnClickListener {
             val x=5
             qwe(x)
         }
-        mButton6 = findViewById(R.id.button6)
         mButton6.setOnClickListener {
             val x=6
             qwe(x)
         }
-        mButton7 = findViewById(R.id.button7)
         mButton7.setOnClickListener {
             val x=7
             qwe(x)
         }
-        mButton8 = findViewById(R.id.button8)
         mButton8.setOnClickListener {
             val x=8
             qwe(x)
         }
-        mButton9 = findViewById(R.id.button9)
         mButton9.setOnClickListener {
             val x=9
             qwe(x)
         }
-        mButton10 = findViewById(R.id.button10)
         mButton10.setOnClickListener {
             val x=10
             qwe(x)
         }
-
     }
 
     private fun qwe(x:Int) {
@@ -89,7 +91,8 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, messageLoss, Toast.LENGTH_SHORT).show()
         }
         mTextView.text = "Ты выиграл $mIndex раз(a)"
-
+        mChislo.text = "Ты выбрал: $x\n" +
+                "Правильный ответ: $rand"
     }
 }
 
