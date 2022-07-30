3079567798390ad2fb8ae5a4b151ee9c489c1259
N0rmalUser <vova.okunev25@gmail.com>
2022-07-31 00:06:00 +0300

    no

--- app/src/main/java/com/androidKazik/MainActivity.kt
+++ app/src/main/java/com/androidKazik/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.androidKazik
 
 import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.TextView
@@ -22,11 +24,17 @@ open class MainActivity : AppCompatActivity() {
     private lateinit var mTextView: TextView
     private lateinit var mChislo: TextView
     private lateinit var mCheckbox:CheckBox
-    var mIndex = 0
-    var q = true
+    private  val WIN_INDEX = "win_index"
+    private  val INDEX = "index"
+    private var mIndex = 0
+    private var q = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate()")
+        if (savedInstanceState != null ) {
+            q = savedInstanceState.getBoolean(INDEX,false)
+        }
         mButton1 = findViewById(R.id.button1)
         mButton1.setOnClickListener { qwe(1) }
         mButton2 = findViewById(R.id.button2)
@@ -46,7 +54,7 @@ open class MainActivity : AppCompatActivity() {
         mButton9 = findViewById(R.id.button9)
         mButton9.setOnClickListener { qwe(9) }
         mButton10 = findViewById(R.id.button10)
-        mButton10.setOnClickListener { qwe(10) }
+        mButton10.setOnClickListener { qwe(0) }
         mChislo = findViewById(R.id.chislo)
         mTextView = findViewById(R.id.textview)
         mCheckbox = findViewById(R.id.checkBox)
@@ -58,8 +66,36 @@ open class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart()")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause()")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume()")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop()")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy()")
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.i(TAG, "onSaveInstanceState")
+        outState.putInt(WIN_INDEX, mIndex)
+        outState.putBoolean(INDEX, q)
+    }
+
     @SuppressLint("SetTextI18n")
     fun qwe(x: Int) {
+        Log.d(TAG, "Кнопка нажата")
         val rand = (1..10).random()
         if (q){
             if (x == rand) {
@@ -74,7 +110,7 @@ open class MainActivity : AppCompatActivity() {
             }
         }
         when (mIndex) {// мне было лень придумывать или искать функцию для определения, будет ли в конце слова стоять "а" или нет.
-            2, 3, 4, 22, 23, 24, -> "Ты выиграл $mIndex раза"
+            2, 3, 4, 22, 23, 24 -> "Ты выиграл $mIndex раза"
             0, 1, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 25, 26, 27, 28, 29, 30 -> "Ты выиграл $mIndex раз"
             else -> ("Ты выиграл $mIndex раз(а)")
         }.also { mTextView.text = it }
